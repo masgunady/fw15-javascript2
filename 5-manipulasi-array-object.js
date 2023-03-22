02ce4fae0ef3b2153bcb21f1b25f77b16c3d28b7
Gunadi PS <gndsulistiawan@gmail.com>
2023-03-22 22:38:29 +0700

    Mengerjakan tugas week3-js2-no5

---
+++ 5-manipulasi-array-object.js
@@ -0,0 +1,36 @@
+let data = {
+    id: 1,
+    name: 'Leanne Graham',
+    usernam: 'Bret',
+    email: 'Sincere@april.biz',
+    address: {
+        street: 'Kulas Light',
+        suite: 'Apt. 556',
+        city: 'Gwenborough',
+        zipcode: '92998-3874',
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org'
+}
+
+let dataNew = {
+    ...data
+};
+
+dataNew.name = 'Gunadi Pratama Sulistiawan';
+dataNew.email = 'gunadips@gmail.com';
+dataNew.hobby = ['run', 'swimming', 'football']
+
+console.log(dataNew);
+
+
+console.log("Get data street and city :");
+const {
+    address: {
+        street,
+        city
+    }
+} = dataNew;
+
+console.log(`Street: ${street}`);
+console.log(`City: ${city}`);
