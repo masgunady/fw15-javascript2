f3bdf65ab5ca0539aabe32c0315a0c57c30fb2a0
Gunadi PS <gndsulistiawan@gmail.com>
2023-03-23 21:27:01 +0700

    mengedit tugas no-3

--- 3-searh-name-array.js
+++ 3-searh-name-array.js
@@ -1,13 +1,13 @@
-function displayLimitName(querySearch, limitSearch, callback) {
+function displayLimitName(querySearch, limitSearch, display) {
     const name = ["Abigail", "Alexandra", "Alison", "Amanda", "Angela", "Bella", "Carol", "Caroline", "Carolyn", "Deirdre", "Diana", "Elizabeth", "Ella", "Faith", "Olivia", "Penelope"];
     let queryRegExp = new RegExp(querySearch, "ig");
     const searchName = name.filter(x => x.match(queryRegExp));
     const displayLimit = searchName.filter((x, y) => y < limitSearch);
-    callback(displayLimit);
+    display(displayLimit);
 }
 
-function show(showData) {
+function callback(showData) {
     console.log(showData);
 }
 
-displayLimitName("an", 3, show);
+displayLimitName("an", 3, callback);
