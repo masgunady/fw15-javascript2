6aba264affed29781548040d11e5be51e21a51da
Gunadi PS <gndsulistiawan@gmail.com>
2023-03-27 16:52:49 +0700

    menambahkan comment code yang terhapus karena conflict, code yang menggunakan regex

--- 3-searh-name-array.js
+++ 3-searh-name-array.js
@@ -10,3 +10,36 @@ function show(showData) {
 }
 
 displayLimitName("an", 3, show);
+
+//code dengan pencarian match
+
+// function displayLimitName(querySearch, limitSearch, callback) {
+//     const name = ["Abigail", "Alexandra", "Alison", "Amanda", "Angela", "Bella", "Carol", "Caroline", "Carolyn", "Deirdre", "Diana", "Elizabeth", "Ella", "Faith", "Olivia", "Penelope"];
+//     const searchName = name.filter(x => x.toLowerCase().match(querySearch));
+//     const displayLimit = searchName.filter((x, y) => y < limitSearch);
+//     callback(displayLimit);
+// }
+
+// function show(showData) {
+//     console.log(showData);
+// }
+
+// displayLimitName("an", 3, show);
+
+
+// code with regular expression
+
+// function displayLimitName(querySearch, limitSearch, callback) {
+//     const name = ["Abigail", "Alexandra", "Alison", "Amanda", "Angela", "Bella", "Carol", "Caroline", "Carolyn", "Deirdre", "Diana", "Elizabeth", "Ella", "Faith", "Olivia", "Penelope"];
+//     let queryRegExp = new RegExp(querySearch, "ig");
+
+//     const searchName = name.filter(x => x.match(queryRegExp));
+//     const displayLimit = searchName.filter((x, y) => y < limitSearch);
+//     callback(displayLimit);
+// }
+
+// function show(showData) {
+//     console.log(showData);
+// }
+
+// displayLimitName("an", 3, show);
